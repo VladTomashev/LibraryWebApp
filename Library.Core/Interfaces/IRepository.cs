9dc730ef53057bf06a51a380bc941e701628298a
VladTomashev <tomashev.vladislav@gmail.com>
2024-10-09 03:17:30 +0300

    Add Interfaces to Core

---
+++ Library.Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Core.Entities;
+
+namespace Library.Core.Interfaces
+{
+    public interface IRepository<T> where T : AbstractEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
