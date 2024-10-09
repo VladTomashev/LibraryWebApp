93e5405ddc273e5b9fd1d7d682578cace982c81c
VladTomashev <tomashev.vladislav@gmail.com>
2024-10-09 03:16:31 +0300

    Add Entities and Enums to Core

---
+++ Library.Core/Entities/BookRental.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Core.Entities
+{
+    public class BookRental : AbstractEntity
+    {
+        public Guid BookId { get; set; }
+        public Guid UserProfileId { get; set; }
+        public DateTime TakingTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public bool IsReturned { get; set; }
+        
+    }
+}
---
+++ Library.Core/Entities/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Core.Entities
+{
+    public class UserProfile : AbstractEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
