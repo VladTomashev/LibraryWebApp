ea069f59ed4efcf7846418084ef9da37e3536b1e
VladTomashev <tomashev.vladislav@gmail.com>
2024-10-10 05:47:10 +0300

    Add mappers to application layer

---
+++ Library.Application/Mappers/BookMapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Library.Application.DTO.Requests;
+using Library.Application.DTO.Responses;
+using Library.Core.Entities;
+
+namespace Library.Application.Mappers
+{
+    public class BookMapper : Profile
+    {
+        public BookMapper()
+        {
+            CreateMap<Book, BookResponse>();
+            CreateMap<BookRequest, Book>();
+            CreateMap<IEnumerable<Book>, IEnumerable<BookResponse>>()
+                .ConvertUsing(books => books.Select(b => new BookResponse
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Isbn = b.Isbn,
+                    Genre = b.Genre,
+                    Description = b.Description,
+                    AuthorId = b.AuthorId,
+                    ImagePath = b.ImagePath
+                }));
+        }
+    }
+}
