2b5d69c46f136a5ce3ab1c723101e741c472bd4f
VladTomashev <tomashev.vladislav@gmail.com>
2024-10-17 20:08:38 +0300

    Add context and configurations to infrastructure layer

---
+++ Library.Infrastructure/EntityFramework/ConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Library.Infrastructure.EntityFramework
+{
+    public static class ConfigurationHelper
+    {
+        public static IConfigurationRoot Configuration { get; }
+
+        static ConfigurationHelper()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static string GetConnectionString(string name)
+        {
+            return Configuration.GetConnectionString(name);
+        }
+    }
+}
---
+++ Library.Infrastructure/EntityFramework/EntitiesConfigurations/AbstractEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.EntityFramework.EntitiesConfigurations
+{
+    public class AbstractEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : AbstractEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
